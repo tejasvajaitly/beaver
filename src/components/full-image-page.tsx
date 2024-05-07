7e71cc754804373c64cc55b5749f4357aba00441
Tejasva Jaitly <tejasvajaitly@gmail.com>
2024-05-06 19:13:21 -0700

    finalize modal and parallel routes

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -1,7 +1,9 @@
+import { clerkClient } from "@clerk/nextjs/server";
 import { getImage } from "~/server/queries";
 
 export default async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
+  const uploaderInfo = await clerkClient.users.getUser(image.userId);
   return (
     <div className="flex h-full w-full min-w-0">
       <div className="flex flex-shrink items-center justify-center">
@@ -10,6 +12,15 @@ export default async function FullPageImageView(props: { id: number }) {
 
       <div className="flex w-48 flex-shrink-0 flex-col border-l">
         <div className="text-xl font-bold"></div>
+        <div className="flex flex-col p-2">
+          <span>Uploaded By</span>
+          <span>{uploaderInfo.fullName}</span>
+        </div>
+
+        <div className="flex flex-col p-2">
+          <span>Created on</span>
+          <span>{new Date(image.createdAt).toLocaleString()}</span>
+        </div>
       </div>
     </div>
   );
