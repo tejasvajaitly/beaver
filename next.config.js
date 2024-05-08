bcf0955c5751b574edd182b2a76ca9d4a59dc0a2
Tejasva Jaitly <tejasvajaitly@gmail.com>
2024-05-07 23:42:05 -0700

    basic posthog analytics

--- next.config.js
+++ next.config.js
@@ -19,6 +19,20 @@ const coreConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://us-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://us.i.posthog.com/:path*",
+      },
+    ];
+  },
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
 };
 
 // Injected content via Sentry wizard below
