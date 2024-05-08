2895df4c6985390017f1ac90369f8db5c43d1085
Tejasva Jaitly <tejasvajaitly@gmail.com>
2024-05-08 01:24:00 -0700

    rate limit with upstash

--- src/server/ratelimit.ts
+++ src/server/ratelimit.ts
@@ -1,5 +1,5 @@
-import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
-import { Redis } from "@upstash/redis"; // see below for cloudflare and fastly adapters
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
 
 // Create a new ratelimiter, that allows 10 requests per 10 seconds
 export const ratelimit = new Ratelimit({
