debecf9ba3d59c7bf8c41bd6104642e1648e5e3f
Tejasva Jaitly <tejasvajaitly@gmail.com>
2024-05-07 23:46:34 -0700

    upload image capture

--- src/app/_components/simple-upload-button.tsx
+++ src/app/_components/simple-upload-button.tsx
@@ -3,6 +3,7 @@
 import { useRouter } from "next/navigation";
 import { useUploadThing } from "~/utils/uploadthing";
 import { toast } from "sonner";
+import { usePostHog } from "posthog-js/react";
 
 // inferred input off useUploadThing
 type Input = Parameters<typeof useUploadThing>;
@@ -73,8 +74,11 @@ function LoadingSvg() {
 export default function SimpleUploadButton() {
   const router = useRouter();
 
+  const posthog = usePostHog();
+
   const { inputProps } = useUploadThingInputProps("imageUploader", {
     onUploadBegin() {
+      posthog.capture("upload-begin");
       toast(
         <div className="flex gap-2 text-white">
           <LoadingSvg /> <span>Uploading...</span>
